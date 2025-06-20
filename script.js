95d43cbfb1647e87c6f6946bffaca8ef7a1c5579
Piyush Kumar <109018436+ramblerpk@users.noreply.github.com>
2025-06-20 18:33:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	s1L = s1.toLowerCase();
+	s2L = s2.toLowerCase();
+	return s1L.indexOf(s2L);
 }
 
 // Please do not change the code below
